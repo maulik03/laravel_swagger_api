8ccde65958984c032cc3959bb52fc3d34dc4cc03
maulik03 <maulikparikh11@gmail.com>
2023-07-03 10:28:14 -0400

    initial commit

---
+++ database/migrations/2023_07_03_140947_create_todo_list_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_list', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('description');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->boolean('priority')->default('false');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_list');
+    }
+};
